6bb2aee9f09fd38a9f395aa801ae7a44195a6f03
Kaizen_0z <yash.amberkar005@gmail.com>
2024-04-15 21:04:04 +0530

    Modified Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -31,7 +31,7 @@ library.add(
 export const Navbar = () => {
 	return (
 		<>
-			<div className='p-4    flex justify-between items-center text-lavender border-b border-b-lavender font-Raleway font-bold'>
+			<div className='p-4  sticky top-0 bg-black flex justify-between items-center text-lavender border-b border-b-lavender font-Raleway font-bold'>
 				<ul className='flex justify-between w-20 space-x-2 relative items-center '>
 					<li className=''></li>
 					<Sidebar />
