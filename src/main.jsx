4a2dab086e933c392dbcc14e61e60e2874306308
Kaizen_0z <yash.amberkar005@gmail.com>
2024-04-20 08:54:54 +0530

    updated routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import { Login } from "./Pages/Login.jsx";
 import { About } from "./Pages/About.jsx";
+import { Contact } from "./Pages/Contact.jsx";
+import { TC } from "./Pages/TC.jsx";
 import "./index.css";
 
 import { createRoot } from "react-dom/client";
@@ -14,6 +16,8 @@ createRoot(document.getElementById("root")).render(
 				<Route path='/' element={<App />} />
 				<Route path='/login' element={<Login />} />
 				<Route path='/about' element={<About />} />
+				<Route path='/contact' element={<Contact />} />
+				<Route path='/terms&conditions' element={<TC />} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
