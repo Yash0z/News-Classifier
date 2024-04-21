54f2cbb988a75f22a5798f01b32354136a24413c
Kaizen_0z <yash.amberkar005@gmail.com>
2024-04-21 20:34:04 +0530

    updated contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,22 +5,22 @@ export const Contact = () => {
 	return (
 		<>
 			<Navbar />
-			<div className=' flex flex-col  space-y-8 p-2 justify-center items-center relative w h-[40vh] top-0 mx-auto  text-center text-4xl sm:text-5xl'>
-				<span className=' text-sky font-Montserrat font-bold '>
+			<div className=' flex  justify-center items-center relative w h-[40vh] text-4xl sm:text-5xl border '>
+				<div className=' text-sky font-Montserrat font-bold bottom-16 relative'>
 					What Can We Help you with?
-				</span>
-				
+				</div>
+				<div className='flex  absolute bottom-28 w-[60%] h-[15%] justify-between'>
 					<form action=''>
 						<textarea
 							name=''
 							id=''
 							cols='30'
-							rows='2'
-							className='w-[85%] text-xl relative h-[8vh]  rounded-lg text-white p-5 bg-black cursor-text outline-none resize-none border-2 duration-200 peer focus: border-gray-500 focus:border-lavender overflow-hidden  md:w-[60vw]  '
+							rows='1'
+							className='w-[88%] text-xl absolute h-[6vh]  rounded-xl text-white p-5 bg-black cursor-text outline-none resize-none border-2 duration-200 peer focus: border-gray-500 focus:border-lavender     '
 						></textarea>
 					</form>
-              
-		
+					<button className='w-[10%] rounded-xl text-2xl bg-sky'>Send</button>
+				</div>
 			</div>
 
 			{/* Quick Access
