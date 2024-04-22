ce7404e1d8122b9818c676721f26456c0c538171
Kaizen_0z <yash.amberkar005@gmail.com>
2024-04-22 18:14:18 +0530

    Added Theme Switcher

--- src/Components/Switch.jsx
+++ src/Components/Switch.jsx
@@ -2,28 +2,31 @@ import React, { useState } from "react";
 import { PiMoonStarsFill } from "react-icons/pi";
 import { TbSunFilled } from "react-icons/tb";
 import { IconContext } from "react-icons";
-export const Switch = () => {
-  const [icon, setIcon] = useState(<PiMoonStarsFill />);
 
-  const handleIcon = () => {
-    if (icon.type === PiMoonStarsFill) {
-      setIcon(<TbSunFilled />);
+export const Switch = () => {
+  const [isLightTheme, setIsLightTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setIsLightTheme((prevTheme) => !prevTheme);
+    const bodyClassList = document.body.classList;
+    if (isLightTheme) {
+      bodyClassList.remove("light");
+      bodyClassList.add("dark");
     } else {
-      setIcon(<PiMoonStarsFill />);
+      bodyClassList.remove("dark");
+      bodyClassList.add("light");
     }
   };
 
   return (
-    <>
     <IconContext.Provider value={{ size: "1.5rem" }}>
       <button
-        className="bg-slate-800 rounded-full p-1"
-        onClick={handleIcon}
+        className="bg-background rounded-full p-1 "
+        onClick={toggleTheme}
       >
-        {icon}
+        {isLightTheme ? <TbSunFilled /> : <PiMoonStarsFill />}
       </button>
-       </IconContext.Provider>
-    </>
+    </IconContext.Provider>
   );
 };
 
