c9242ee05230f1819415f46bc08cc171ebfa63bd
Kaizen_0z <yash.amberkar005@gmail.com>
2024-04-24 19:53:54 +0530

    Locally Store Theme

--- src/Components/Switch.jsx
+++ src/Components/Switch.jsx
@@ -1,27 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { PiMoonStarsFill } from "react-icons/pi";
 import { TbSunFilled } from "react-icons/tb";
 import { IconContext } from "react-icons";
 
 export const Switch = () => {
   const [isLightTheme, setIsLightTheme] = useState(true);
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    setIsLightTheme(storedTheme !== "dark"); 
+  }, []);
 
   const toggleTheme = () => {
-    setIsLightTheme((prevTheme) => !prevTheme);
+    const newTheme = !isLightTheme;
+    setIsLightTheme(newTheme);
+    localStorage.setItem("theme", newTheme ? "light" : "dark");
+  };
+
+  useEffect(() => {
     const bodyClassList = document.body.classList;
     if (isLightTheme) {
-      bodyClassList.remove("light");
-      bodyClassList.add("dark");
-    } else {
       bodyClassList.remove("dark");
       bodyClassList.add("light");
+    } else {
+      bodyClassList.remove("light");
+      bodyClassList.add("dark");
     }
-  };
+  }, [isLightTheme]);
 
   return (
     <IconContext.Provider value={{ size: "1.5rem" }}>
       <button
-        className="bg-background rounded-full p-1 "
+        className="bg-background rounded-full p-1"
         onClick={toggleTheme}
       >
         {isLightTheme ? <TbSunFilled /> : <PiMoonStarsFill />}
